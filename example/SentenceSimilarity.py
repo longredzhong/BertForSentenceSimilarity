7ef49c2ab25269c8e7d59b3c1ec0cd6d773bc814
longred <774931894@qq.com>
2020-05-04 00:01:29 +0800

    add ESIM
    Signed-off-by: longred <774931894@qq.com>

--- example/SentenceSimilarity.py
+++ example/SentenceSimilarity.py
@@ -8,8 +8,9 @@ import torch
 from model.AlbertForSequenceClassification import AlbertForSequenceClassification
 from utils.tokenizers import Tokenizer
 if __name__ == "__main__":
+    # use albert
     net = AlbertForSequenceClassification.from_pretrained(
-        "/home/longred/BertForSentenceSimilarity/output/LCQMC")
+        "/home/longred/BertForSentenceSimilarity/output/LCQMC/albert/")
     tokenizer = Tokenizer(
         "/home/longred/BertForSentenceSimilarity/prev_trained_model/albert_tiny_zh/vocab.txt")
     sentence_1 = "这腰带是什么牌子"
@@ -19,5 +20,20 @@ if __name__ == "__main__":
                 token_type_ids= torch.tensor(segment_ids).unsqueeze(0))
     print(out)
 
-
-# %%
+    # use ESIM
+    net = AlbertForSequenceClassification.from_pretrained(
+        "/home/longred/BertForSentenceSimilarity/output/LCQMC/ESIM/")
+    tokenizer = Tokenizer(
+        "/home/longred/BertForSentenceSimilarity/prev_trained_model/albert_tiny_zh/vocab.txt")
+    sentence_1 = "这腰带是什么牌子"
+    sentence_2 = "护腰带什么牌子好"
+    input_1_ids = tokenizer.encode(sentence_1)[0]
+    input_1_len = len(input_1_ids)
+    input_2_ids = tokenizer.encode(sentence_2)[0]
+    input_2_len = len(input_2_ids)
+    out = net(input_1=input_1_ids,
+              input_1_lengths=input_1_len,
+              input_2=input_2_ids,
+              input_2_lengths=input_2_len
+             )
+    print(out)
