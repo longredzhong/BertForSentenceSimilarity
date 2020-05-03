496f20594b712e74355db22f6c045504a8c5cc62
longred <774931894@qq.com>
2020-05-03 17:40:02 +0800

    add sequence_padding add toTensor
    Signed-off-by: longred <774931894@qq.com>

---
+++ utils/util.py
@@ -0,0 +1,21 @@
+import numpy as np
+import torch
+def sequence_padding(inputs, length=None, padding=0):
+    """Numpy函数，将序列padding到同一长度
+    """
+    if length is None:
+        length = max([len(x) for x in inputs])
+
+    pad_width = [(0, 0) for _ in np.shape(inputs[0])]
+    outputs = []
+    for x in inputs:
+        x = x[:length]
+        pad_width[0] = (0, length - len(x))
+        x = np.pad(x, pad_width, 'constant', constant_values=padding)
+        outputs.append(x)
+
+    return torch.tensor(outputs, dtype=torch.long)
+
+
+def toTensor(inputs):
+    return torch.tensor(inputs)
